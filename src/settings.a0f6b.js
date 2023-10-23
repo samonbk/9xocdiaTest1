508c8358544c240ad24f63d2da3108c71a245ab1
brothong2000 <136555363+samonbk@users.noreply.github.com>
2023-10-23 15:17:21 +0700

    test1

---
+++ src/settings.a0f6b.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scenes/Game.fire",orientation:"landscape",jsList:[],bundleVers:{internal:"69b9a",main:"70e68"}};
